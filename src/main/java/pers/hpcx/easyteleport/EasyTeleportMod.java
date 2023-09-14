bd550cc8c42792054b6876083a1223100cf40330
LittleAmber2005 <2975380349@qq.com>
2023-09-14 17:13:09 +0800

    commit

--- src/main/java/pers/hpcx/easyteleport/EasyTeleportMod.java
+++ src/main/java/pers/hpcx/easyteleport/EasyTeleportMod.java
@@ -231,7 +231,7 @@ public class EasyTeleportMod implements ModInitializer, ServerLifecycleEvents.Se
         requestList.add(new TeleportRequest(sourceID, targetID, requestTimeout / 50));
         send(sourcePlayer, true, green("Requested to teleport to "), player(targetPlayer), green("."));
         send(targetPlayer, true, player(sourcePlayer), green(" has requested to teleport to you. Type "), yellow("/tpaccept"), green(" to accept."));
-        targetPlayer.playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.PLAYERS, 1.0f, 1.0f);
+        targetPlayer.playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.PLAYERS, 2.0f, 1.0f);
         return 1;
     }
     
@@ -253,7 +253,7 @@ public class EasyTeleportMod implements ModInitializer, ServerLifecycleEvents.Se
         requests2.put(sourceID, new TeleportRequest(sourceID, targetID, requestTimeout / 50));
         send(sourcePlayer, true, player(targetPlayer), green(" has requested to teleport you to there. Type "), yellow("/tpaccept"), green(" to accept."));
         send(targetPlayer, true, green("Requested to teleport "), player(sourcePlayer), green(" to you."));
-        sourcePlayer.playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.PLAYERS, 1.0f, 1.0f);
+        sourcePlayer.playSound(SoundEvents.BLOCK_NOTE_BLOCK_BELL.value(), SoundCategory.PLAYERS, 2.0f, 1.0f);
         return 1;
     }
     
